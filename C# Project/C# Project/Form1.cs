958e8254589c7e10155ec26cd2784c3b7467253d
M2D007 <Mohammed007alabri@gmail.com>
2025-03-23 21:53:42 +0400

    m2d

--- C# Project/C# Project/Form1.cs
+++ C# Project/C# Project/Form1.cs
@@ -373,7 +373,7 @@ namespace C__Project
 			dblftotalveg = ((dblftotalveg * intperiod) / 1000);
 			dblmtotalveg = ((dblmtotalveg * intperiod) / 1000);
 
-
+// hgfyufyufghfhgf
 
 			dbltotalcups = (dblftotalfruit + dblftotalveg) + (dblmtotalfruit + dblmtotalveg);
 
